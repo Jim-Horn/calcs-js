2de5ab453037fa84a744e76b15998586322f9f7d
Jim Horn <jim@jdhorn.com>
2020-08-08 19:55:04 -0700

    Returning an object instead of just an error message makes more sense

--- components/Calcs/calcs.js
+++ components/Calcs/calcs.js
@@ -1,19 +1,21 @@
 import { mathFns } from '../utils';
 export default function (arr) {
     if (!Array.isArray(arr)) {
-        return `expected an Array, received ${typeof arr}: ${arr}`;
+        return { error: `expected an Array, received ${typeof arr}: ${arr}` };
     }
 
     if (arr.length !== 13) {
-        return `expected an Array with 13 elements, received ${arr.length}: ${JSON.stringify(arr)}`;
+        return { error: `expected an Array with 13 elements, received ${arr.length}: ${JSON.stringify(arr)}` };
     }
 
     if (arr.some((i) => !Number.isInteger(i))) {
-        return `expected all Array elements to be integers, received: ${JSON.stringify(arr)}`;
+        return { error: `expected all Array elements to be integers, received: ${JSON.stringify(arr)}` };
     }
 
     if (arr.some((i) => i < 1 || i > 7)) {
-        return `expected all Array elements to be greater than 0 and less than 8, received: ${JSON.stringify(arr)}`;
+        return {
+            error: `expected all Array elements to be greater than 0 and less than 8, received: ${JSON.stringify(arr)}`,
+        };
     }
 
     return {
--- components/Calcs/calcs.test.js
+++ components/Calcs/calcs.test.js
@@ -2,15 +2,15 @@ import calcs from './calcs';
 
 describe('calcs - basic argument tests', () => {
     test('expects an array as a parameter', () => {
-        expect(calcs()).toEqual(`expected an Array, received undefined: undefined`);
-        expect(calcs('foo')).toBe('expected an Array, received string: foo');
-        expect(calcs(5)).toBe(`expected an Array, received number: 5`);
-        expect(calcs({})).toBe(`expected an Array, received object: [object Object]`);
+        expect(calcs()).toEqual({ error: `expected an Array, received undefined: undefined` });
+        expect(calcs('foo')).toEqual({ error: `expected an Array, received string: foo` });
+        expect(calcs(5)).toEqual({ error: `expected an Array, received number: 5` });
+        expect(calcs({})).toEqual({ error: `expected an Array, received object: [object Object]` });
     });
 
     test('expects the array to contain 13 elements', () => {
-        expect(calcs([1])).toBe('expected an Array with 13 elements, received 1: [1]');
-        expect(calcs([1, 5, 'a'])).toBe(`expected an Array with 13 elements, received 3: [1,5,\"a\"]`);
+        expect(calcs([1])).toEqual({ error: `expected an Array with 13 elements, received 1: [1]` });
+        expect(calcs([1, 5, 'a'])).toEqual({ error: `expected an Array with 13 elements, received 3: [1,5,\"a\"]` });
     });
 
     test('expects all array elements to be integers', () => {
@@ -20,13 +20,13 @@ describe('calcs - basic argument tests', () => {
             stdDev: 1.8873606379054304,
         });
 
-        expect(calcs([1, 2, 3, 4.6, 5, 6, 7, 8, 9, 10, 11, 12, 13])).toEqual(
-            'expected all Array elements to be integers, received: [1,2,3,4.6,5,6,7,8,9,10,11,12,13]'
-        );
+        expect(calcs([1, 2, 3, 4.6, 5, 6, 7, 8, 9, 10, 11, 12, 13])).toEqual({
+            error: `expected all Array elements to be integers, received: [1,2,3,4.6,5,6,7,8,9,10,11,12,13]`,
+        });
 
-        expect(calcs([1, 2, 3, 4, 5, 6, 'a', 8, 9, 10, 11, 12, 13])).toEqual(
-            `expected all Array elements to be integers, received: [1,2,3,4,5,6,"a",8,9,10,11,12,13]`
-        );
+        expect(calcs([1, 2, 3, 4, 5, 6, 'a', 8, 9, 10, 11, 12, 13])).toEqual({
+            error: `expected all Array elements to be integers, received: [1,2,3,4,5,6,"a",8,9,10,11,12,13]`,
+        });
     });
 
     test('expects all array elements to be in the range of 1-7', () => {
@@ -35,11 +35,11 @@ describe('calcs - basic argument tests', () => {
             mean: 3.769230769230769,
             stdDev: 1.8873606379054304,
         });
-        expect(calcs([0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2])).toEqual(
-            `expected all Array elements to be greater than 0 and less than 8, received: [0,1,2,3,4,5,6,7,6,5,4,3,2]`
-        );
-        expect(calcs([1, 2, 3, 4, 5, 6, 7, 8, 6, 5, 4, 3, 2])).toEqual(
-            `expected all Array elements to be greater than 0 and less than 8, received: [1,2,3,4,5,6,7,8,6,5,4,3,2]`
-        );
+        expect(calcs([0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2])).toEqual({
+            error: `expected all Array elements to be greater than 0 and less than 8, received: [0,1,2,3,4,5,6,7,6,5,4,3,2]`,
+        });
+        expect(calcs([1, 2, 3, 4, 5, 6, 7, 8, 6, 5, 4, 3, 2])).toEqual({
+            error: `expected all Array elements to be greater than 0 and less than 8, received: [1,2,3,4,5,6,7,8,6,5,4,3,2]`,
+        });
     });
 });
